30807552234666fc76f5829a3d347163df5a2a7e
nataliayikewang <natalia.yike.wang@estudiantat.upc.edu>
2025-05-18 13:01:15 +0200

    high score record en proceso

--- My project/Assets/Scripts/UI/ScoreDisplay.cs
+++ My project/Assets/Scripts/UI/ScoreDisplay.cs
@@ -6,10 +6,31 @@ public class ScoreDisplay : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public int score = 0;
 
+    private int highScore = 0;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        Debug.Log("Record cargado: " + highScore);
+    }
+
     public void AddPoints(int points)
     {
         score += points;
         scoreText.text = score.ToString();
         Debug.Log("Puntos sumados: " + points + ", nuevo puntaje: " + score);
+
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save(); // Opcional, para asegurarte que se guarde inmediatamente
+            Debug.Log("Nuevo récord: " + highScore);
+        }
+    }
+
+    public int GetHighScore()
+    {
+        return highScore;
     }
 }
